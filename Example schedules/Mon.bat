71345b0d63ac8fccbddad5de4d7ce8cbc9d0890b
aelrou <mike@aelrou.net>
2020-08-28 17:47:47 -0600

    Fix bug in batch example where some systems ignore /wait

--- Example schedules/Mon.bat
+++ Example schedules/Mon.bat
@@ -1,5 +1,5 @@
 rem SERVERONE
-start "powershell.exe" /wait /b "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -File "C:\HyperV_Export.ps1" -VMID "9623d59a-a9e9-40cf-a0fd-913248491d50"
+start "Z" /b /wait "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -File "C:\HyperV_Export.ps1" -VMID "9623d59a-a9e9-40cf-a0fd-913248491d50"
 
 rem SERVERTWO
-start "powershell.exe" /wait /b "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -File "C:\HyperV_Export.ps1" -VMID "623d59a9-9e9a-0cf4-0fda-13248491d509"
+start "Z" /b /wait "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -File "C:\HyperV_Export.ps1" -VMID "623d59a9-9e9a-0cf4-0fda-13248491d509"
--- Example schedules/Sun.bat
+++ Example schedules/Sun.bat
@@ -1,5 +1,5 @@
 rem SERVERNINE
-start "powershell.exe" /wait /b "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -File "C:\HyperV_Export.ps1" -VMID "7623d59a-39e9-40cf-50fd-713248491d50"
+start "Z" /b /wait "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -File "C:\HyperV_Export.ps1" -VMID "7623d59a-39e9-40cf-50fd-713248491d50"
 
 rem SERVERTEN
-start "powershell.exe" /wait /b "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -File "C:\HyperV_Export.ps1" -VMID "423d59a9-7e9a-4cf4-4fda-53248491d509"
+start "Z" /b /wait "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -File "C:\HyperV_Export.ps1" -VMID "423d59a9-7e9a-4cf4-4fda-53248491d509"
