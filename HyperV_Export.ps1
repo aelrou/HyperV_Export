f349a2e2614db290bc0b7c897e9ad915f16dd3e0
aelrou <mike@aelrou.net>
2022-02-25 23:21:45 -0700

    Added methods for live backup when possible

--- HyperV_Export.ps1
+++ HyperV_Export.ps1
@@ -1,4 +1,6 @@
 # Set-ExecutionPolicy RemoteSigned
+# Unblock-File -Path "C:\HyperV_Export.ps1"
+# "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -File "C:\HyperV_Export.ps1"
 Param ([string]$VMID)
 <#  
     $ExportPath is the temporary storage space for uncompressed Hyper-V exports.
@@ -25,7 +27,7 @@ if (!($VMID)) {
     Write-Host "PowerShell> & ""C:\HyperV_Export.ps1"" -VMID ""9623d59a-a9e9-40cf-a0fd-913248491d50"""
     Exit
 }
-$DateTimeStart = Get-Date -format "yyyy-MM-dd-THHmm"
+
 try {
     $VMName = $(Get-VM -Id $VMID -ErrorAction Stop).VMName  
 }
@@ -34,10 +36,27 @@ catch {
     Write-Host $PSItem.ToString()
     Exit
 }
-$LogFile = "$($ExportPath)\$($VMName)_$($DateTimeStart).log"
+
+$FORMAT_ISO_8601 = "yyyy-MM-ddTHHmmss"
+$REGEX_ISO_8601 = "\d{4}-\d{2}-\d{2}T\d{2}\d{2}\d{2}"
+$DateTimeStart = Get-Date -format $FORMAT_ISO_8601 #$DateTimeStart = Get-Date -format "yyyy-MM-dd-THH-mm"
+$LogFile = "$($VMName)_$($DateTimeStart).log"
+
+if (!(Test-Path -Path "$($ExportPath)\$($LogFile)" -PathType Leaf)) {
+    if (!(Test-Path -Path "$($ExportPath)" -PathType Container)) {
+        try {
+            New-Item -Path "$($ExportPath)" -ItemType "directory" -ErrorAction Stop
+        }
+        catch {
+            Write-Host($Error[0].Exception.GetType().FullName)
+            Write-Host($PSItem.ToString())
+            Exit
+        }
+    }
+}
 Function LogWrite {
     Param ([string]$LogString)
-    Add-content $LogFile -value $LogString
+    Add-content "$($ExportPath)\$($LogFile)" -value $LogString
     Write-Host $LogString
 }
 
@@ -65,7 +84,7 @@ while (1) {
     try {
         # I am not using New-PSDrive here because New-PSDrive is bafflingly unreliable with network SMB.
         (New-Object -ComObject WScript.Network).MapNetworkDrive($NetworkDrive, "\\169.254.127.127\Hyper-V Exports", $false, "MyServer\MyUsername", "MyPassword")
-        # TODO - Implement PSCredential for providing the username and password.
+        # TODO - Implement PSCredential for providing the network drive credentials.
     }
     catch {
         LogWrite $($Error[0].Exception.GetType().FullName)
@@ -75,36 +94,80 @@ while (1) {
     $LoopCount ++
 }
 
-try {
-    $State = $(Get-VM -Id $VMID).State
-    if ($(Get-VM -Id $VMID).State -eq "paused") {
+$Upload = $false
+$OriginalState = $(Get-VM -Id $VMID).State
+$DateTimeExport = Get-Date -format $FORMAT_ISO_8601
+
+$LoopCount = 1
+while (1) {
+    try {
+        $CurrentState = $(Get-VM -Id $VMID).State
+        if ($CurrentState -eq "paused") {
+            LogWrite "Resume $($VMName)"
+            Start-VM -Name $VMName -ErrorAction Stop
+        }
+        if ($CurrentState -eq "running") {
+            LogWrite "Live export $($VMName) to ""$($ExportPath)\$($DateTimeExport)"""
+            Export-VM -Name $VMName -Path "$($ExportPath)\$($DateTimeExport)" -ErrorAction Stop
+            $Upload = $true
+            Break
+        }
+        if ($CurrentState -eq "off" -or $CurrentState -eq "saved") {
+            LogWrite "Export $($VMName) to ""$($ExportPath)\$($DateTimeExport)"""
+            Export-VM -Name $VMName -Path "$($ExportPath)\$($DateTimeExport)" -ErrorAction Stop
+            $Upload = $true
+            Break
+        }
+    }
+    # TODO - Implement catch and procedure for non-live exports.
+    catch {
+        LogWrite $($Error[0].Exception.GetType().FullName)
+        LogWrite $($PSItem.ToString())  
+    }
+    if ($LoopCount -gt 3) {
+        LogWrite "Failed to export $($VMName)"
+        Break
+    }
+    else {
+        Start-Sleep -Seconds 1
+        $LoopCount ++    
+    }
+}
+
+if ($OriginalState -eq "running" -and $(Get-VM -Id $VMID).State -ne "running") {
+    try {
         LogWrite "Resume $($VMName)"
         Start-VM -Name $VMName -ErrorAction Stop
     }
-    if ($(Get-VM -Id $VMID).State -eq "running") {
-        LogWrite "Save state of $($VMName)"
-        Save-VM -Name $VMName -ErrorAction Stop
+    catch {
+        LogWrite $($Error[0].Exception.GetType().FullName)
+        LogWrite $($PSItem.ToString())
     }
-    if ($(Get-VM -Id $VMID).State -eq "off" -or $(Get-VM -Id $VMID).State -eq "saved") {
-        $DateTimeExport = Get-Date -format "yyyy-MM-dd-THHmm"
-        LogWrite "Export $($VMName) to ""$($ExportPath)\$($DateTimeExport)"""
-        Export-VM -Name $VMName -Path "$($ExportPath)\$($DateTimeExport)" -ErrorAction Stop
-        if ($State -eq "running") {
-            LogWrite "Startup $($VMName)"
-            Start-VM -Name $VMName -ErrorAction Stop
-        }
-        $DateTimeUpload = Get-Date -format "yyyy-MM-dd-THHmm"
+}                
+
+if ($Upload) {
+    try {
+        $DateTimeUpload = Get-Date -format $FORMAT_ISO_8601
         LogWrite "Encrypt, compress, and upload ""$($VMName)_$($DateTimeUpload).7z"" to drive $($NetworkDrive)\"
-        Start-Process -NoNewWindow -Wait -FilePath "C:\Program Files\7-Zip\7z.exe" -ArgumentList "a", "-ms=on", "-v5119m", "-mx1", "-mmt7", "-p""123456789ABCDEFGHijKLMNoPQRSTUVWXYZ""", """$($NetworkDrive)\$($VMName)\$($VMName)_$($DateTimeUpload).7z""", """$($ExportPath)\$($DateTimeExport)\$($VMName)\*""" -RedirectStandardOutput "$($ExportPath)\$($DateTimeStart)\stdout.log" -RedirectStandardError "$($ExportPath)\$($DateTimeStart)\stderr.log" -ErrorAction Stop
+        Start-Process -NoNewWindow -Wait -FilePath "C:\Program Files\7-Zip\7z.exe" -ArgumentList "a", "-ms=on", "-v5119m", "-mx1", "-mmt7", "-p""123456789ABCDEFGHijKLMNoPQRSTUVWXYZ""", """$($NetworkDrive)\$($VMName)\$($VMName)_$($DateTimeUpload).7z""", """$($ExportPath)\$($DateTimeExport)\$($VMName)\*""" -RedirectStandardOutput "$($ExportPath)\$($DateTimeExport)\stdout.log" -RedirectStandardError "$($ExportPath)\$($DateTimeExport)\stderr.log" -ErrorAction Stop
         # TODO - Implement PSCredential for providing the encryption password.
-        LogWrite (Get-Content -Path "$($ExportPath)\$($DateTimeStart)\stdout.log")
-        $DateTimeStop = Get-Date -format "yyyy-MM-dd-THHmm"
+        LogWrite (Get-Content -Path "$($ExportPath)\$($DateTimeExport)\stdout.log")
+        $DateTimeStop = Get-Date -format $FORMAT_ISO_8601
         LogWrite "Upload ended $($DateTimeStop)"
+    }
+    catch {
+        LogWrite $($Error[0].Exception.GetType().FullName)
+        LogWrite $($PSItem.ToString())
+    }    
+}
+
+if ($Upload) {
+    try {
         LogWrite "Cleanup ""$($ExportPath)\$($DateTimeExport)""" 
         Remove-Item "$($ExportPath)\$($DateTimeExport)" -Recurse -Force -Confirm:$false -ErrorAction Stop
     }
-}
-catch {
-    LogWrite $($Error[0].Exception.GetType().FullName)
-    LogWrite $($PSItem.ToString())
+    catch {
+        LogWrite $($Error[0].Exception.GetType().FullName)
+        LogWrite $($PSItem.ToString())
+    }    
 }
